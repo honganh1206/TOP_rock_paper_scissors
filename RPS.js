f5b152ae2c366a87f41741edb57e10d76abcbad5
Hong Anh <honganh@example.com>
2022-03-08 23:45:37 +0700

    Practicing with git branch

--- RPS.js
+++ RPS.js
@@ -29,47 +29,47 @@ let roundWinner = '';
 // game();
 
 //functions
-function computerPlay () {
-    let choice = CHOICES[Math.floor(Math.random()*CHOICES.length)];
-    return choice;
+function computerPlay() {
+  let choice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
+  return choice;
 }
 
 function capitalizeChoice(playerSelection) {
-    let capChoice = playerSelection.toLowerCase();
-    capChoice = capChoice.charAt(0).toUpperCase() + capChoice.substring(1);
-    return capChoice;
+  let capChoice = playerSelection.toLowerCase();
+  capChoice = capChoice.charAt(0).toUpperCase() + capChoice.substring(1);
+  return capChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection !== computerSelection) {
-        if (playerSelection == CHOICES[0] && computerSelection == CHOICES [1]) {
-            computerScore++;
-            return RESULT[0];
-        }
-        else if (playerSelection == CHOICES[0] && computerSelection == CHOICES [2]) {
-            playerScore++;
-            return RESULT[1];
-        }
-        if (playerSelection == CHOICES[1] && computerSelection == CHOICES [2]) {
-            computerScore++;
-            return RESULT[0];
-        }
-        else if (playerSelection == CHOICES[1] && computerSelection == CHOICES [0]) {
-            playerScore++;
-            return RESULT[1];
-        }
-        if (playerSelection == CHOICES[2] && computerSelection == CHOICES [0]) {
-            computerScore++;
-            return RESULT[0];
-        }
-        else if (playerSelection == CHOICES[2] && computerSelection == CHOICES [1]) {
-            playerScore++;
-            return RESULT[1];
-        }
+  if (playerSelection !== computerSelection) {
+    if (playerSelection == CHOICES[0] && computerSelection == CHOICES[1]) {
+      computerScore++;
+      return RESULT[0];
     }
-    else {
-        return RESULT[2];
+    else if (playerSelection == CHOICES[0] && computerSelection == CHOICES[2]) {
+      playerScore++;
+      return RESULT[1];
     }
+    if (playerSelection == CHOICES[1] && computerSelection == CHOICES[2]) {
+      computerScore++;
+      return RESULT[0];
+    }
+    else if (playerSelection == CHOICES[1] && computerSelection == CHOICES[0]) {
+      playerScore++;
+      return RESULT[1];
+    }
+    if (playerSelection == CHOICES[2] && computerSelection == CHOICES[0]) {
+      computerScore++;
+      return RESULT[0];
+    }
+    else if (playerSelection == CHOICES[2] && computerSelection == CHOICES[1]) {
+      playerScore++;
+      return RESULT[1];
+    }
+  }
+  else {
+    return RESULT[2];
+  }
 }
 
 
@@ -127,114 +127,114 @@ overlay.addEventListener('click', closeEndgameModal)
 //     }
 // }
 
-function gameOver (){
-    return computerScore == 5 || playerScore == 5;
+function gameOver() {
+  return computerScore == 5 || playerScore == 5;
 }
 
 function handleClick(playerSelection) {
-    // check if game ends, otherwise continue
-    if (gameOver()) {
-        openEndgameModal();
-        return;
-    }
-    
-    const computerSelection =  computerPlay(); // why const here? 
-    playRound(playerSelection, computerSelection);
-    updateChoices(playerSelection, computerSelection);
-    updateScore();
+  // check if game ends, otherwise continue
+  if (gameOver()) {
+    openEndgameModal();
+    return;
+  }
 
-    if (gameOver()) {
-        openEndgameModal();
-        setFinalMessage(); // show result in the modal
-    }
+  const computerSelection = computerPlay(); // why const here? 
+  playRound(playerSelection, computerSelection);
+  updateChoices(playerSelection, computerSelection);
+  updateScore();
+
+  if (gameOver()) {
+    openEndgameModal();
+    setFinalMessage(); // show result in the modal
+  }
 }
 
 // show choices of both player and computer on the UI
 function updateChoices(playerSelection, computerSelection) {
-    switch (playerSelection) { 
-        case CHOICES[0]:
-            playerSign.textContent = '🪨'
-            break
-        case CHOICES[1]:
-            playerSign.textContent = '🧻'
-            break
-        case CHOICES[2]:
-            playerSign.textContent = '✂️'
-            break
-    }
+  switch (playerSelection) {
+    case CHOICES[0]:
+      playerSign.textContent = '🪨'
+      break
+    case CHOICES[1]:
+      playerSign.textContent = '🧻'
+      break
+    case CHOICES[2]:
+      playerSign.textContent = '✂️'
+      break
+  }
 
-    switch (computerSelection) {
-        case CHOICES[0]:
-            computerSign.textContent = '🪨';
-            break;
-        case CHOICES[1]:
-            computerSign.textContent = '🧻';
-            break;
-        case CHOICES[2]:
-            computerSign.textContent = '✂️';
-            break;
-    }
+  switch (computerSelection) {
+    case CHOICES[0]:
+      computerSign.textContent = '🪨';
+      break;
+    case CHOICES[1]:
+      computerSign.textContent = '🧻';
+      break;
+    case CHOICES[2]:
+      computerSign.textContent = '✂️';
+      break;
+  }
 }
 //  show winner + update score after each round
 function updateScore() {
-    if (roundWinner === RESULT[2]) {
-        scoreInfo.textContent = "It's a tie!";
-    }
-    else if (roundWinner === RESULT[1]) {
-        scoreInfo.textContent = 'You won this round!';
-    }
-    else if (roundWinner === RESULT[0]) {
-        scoreInfo.textContent = 'Computer won this round.'
-    }
+  if (roundWinner === RESULT[2]) {
+    scoreInfo.textContent = "It's a tie!";
+  }
+  else if (roundWinner === RESULT[1]) {
+    scoreInfo.textContent = 'You won this round!';
+  }
+  else if (roundWinner === RESULT[0]) {
+    scoreInfo.textContent = 'Computer won this round.'
+  }
 
-    playerScorePara.textContent = `Player: ${playerScore}`;
-    computerScorePara.textContent = `Computer: ${computerScore}`;
+  playerScorePara.textContent = `Player: ${playerScore}`;
+  computerScorePara.textContent = `Computer: ${computerScore}`;
 
 }
 
-function updateScoreMessage (winner, playerSelection, computerSelection) {
-    if (winner == RESULT[1]) {
-        rule.textContent = `${capitalizeChoice(playerSelection)} 
-        beats ${computerSelection.toLowerCase()}`;
-        return;
-    }
-    if (winner == RESULT[0]) {
-        rule.textContent = `${capitalizeChoice(playerSelection)} 
-        is beaten by ${computerSelection.toLowerCase()}`;
-        return;
-    }
+function updateScoreMessage(winner, playerSelection, computerSelection) {
+  if (winner == RESULT[1]) {
     rule.textContent = `${capitalizeChoice(playerSelection)} 
+        beats ${computerSelection.toLowerCase()}`;
+    return;
+  }
+  if (winner == RESULT[0]) {
+    rule.textContent = `${capitalizeChoice(playerSelection)} 
+        is beaten by ${computerSelection.toLowerCase()}`;
+    return;
+  }
+  rule.textContent = `${capitalizeChoice(playerSelection)} 
     ties with by ${computerSelection.toLowerCase()}`;
 }
 
 // turn on/off modal + blur blackground
 function openEndgameModal() {
-    endgameModal.classList.add('active');
-    overlay.classList.add('active');
+  endgameModal.classList.add('active');
+  overlay.classList.add('active');
 }
 function closeEndgameModal() {
-    endgameModal.classList.remove('active');
-    overlay.classList.remove('active');
+  endgameModal.classList.remove('active');
+  overlay.classList.remove('active');
 }
 
 function setFinalMessage() {
-    //complex if structure
-    return playerScore > computerScore 
-        ? (endgameMsg.textContent = 'You won!') // if true
-        : (endgameMsg.textContent = 'You lost...') // else
+  //complex if structure
+  return playerScore > computerScore
+    ? (endgameMsg.textContent = 'You won!') // if true
+    : (endgameMsg.textContent = 'You lost...') // else
 }
 
 function restartGame() {
-    playerScore = 0;
-    computerScore = 0;
-    scoreInfo.textContent = 'Your move:';
-    rule.textContent = 'After 5 rows, either you or the computer wins.';
-    playerScorePara.textContent = 'Player: 0';
-    computerScorePara.textContent = 'Computer: 0';
-    playerSign.textContent = '❔';
-    computerSign.textContent = '❔';
-    endgameModal.classList.remove('active');
-    overlay.classList.remove('active');
+  playerScore = 0;
+  computerScore = 0;
+  scoreInfo.textContent = 'Your move:';
+  rule.textContent = 'After 5 rows, either you or the computer wins.';
+  playerScorePara.textContent = 'Player: 0';
+  computerScorePara.textContent = 'Computer: 0';
+  playerSign.textContent = '❔';
+  computerSign.textContent = '❔';
+  endgameModal.classList.remove('active');
+  overlay.classList.remove('active');
 }
 
 
